4d74d35a1e0691b88499c2ae5da2ce4700feab16
Alexander Polakov <polachok@gmail.com>
2011-02-23 20:13:27 +0300

    Window -> UI, add eprint, add drawing widget

--- ej.c
+++ ej.c
@@ -6,9 +6,10 @@
 #include <libdjvu/miniexp.h>
 #include <libdjvu/ddjvuapi.h>
 
-struct Window {
+struct UI {
 	GtkWidget *window;
-} Window;
+	GtkWidget *docarea;
+} UI;
 
 struct Document {
 	char *filename;
@@ -16,7 +17,29 @@ struct Document {
 	ddjvu_document_t *doc;
 } Document;
 
-int main(int argc, char *argv[])
+void
+eprint(const char *errstr, ...)
 {
+    va_list ap;
+
+    va_start(ap, errstr);
+    vfprintf(stderr, errstr, ap);
+    va_end(ap);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+	gtk_init(&argc, &argv);
+	if (argc == 2)
+		Document.filename = argv[1];
+	else 
+		eprint("usage: ej [file]\n");
+
+	UI.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	UI.docarea = gtk_image_new();
+	g_signal_connect (G_OBJECT (UI.window), "destroy",
+			G_CALLBACK (gtk_main_quit), NULL);
+	gtk_widget_show_all(UI.window);
+	gtk_main();
 	return 0;
 }
